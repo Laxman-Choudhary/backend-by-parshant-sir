5fab611a04b69d9ae6b7998653fa5f058b1584cc
Laxman-Choudhary <ramlaxman405852@gmail.com>
2025-11-17 22:04:24 +0530

    first commit

--- Chapter 12 - Dynamic UI using EJS/app.js
+++ Chapter 12 - Dynamic UI using EJS/app.js
@@ -6,10 +6,14 @@ const express = require('express');
 
 //Local Module
 const userRouter = require("./routes/userRouter")
-const hostRouter = require("./routes/hostRouter")
+const {hostRouter} = require("./routes/hostRouter")
 const rootDir = require("./utils/pathUtil");
 
 const app = express();
+
+app.set('view engine','ejs');
+app.set('views','views');
+
 
 app.use(express.urlencoded());
 app.use(userRouter);
@@ -18,10 +22,10 @@ app.use("/host", hostRouter);
 app.use(express.static(path.join(rootDir, 'public')))
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
